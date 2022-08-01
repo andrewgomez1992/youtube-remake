7ba13df162894f47de73555483d3a7494f08ea35
Andrew Gomez <drewgomez209@gmail.com>
2022-08-01 12:26:07 -0700

    mongo db connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express()
+dotenv.config()
+
+const connect = () => {
+    mongoose.connect(process.env.MONGO)
+        .then(() => {
+            console.log('Connected to Mongo!')
+        })
+        .catch(err => {
+            console.log('Mongo DB not connected!')
+        })
+}
 
 app.listen(9000, () => {
-    console.log('Server All Good!')
+    connect()
+    console.log('Connected to server!')
 })
