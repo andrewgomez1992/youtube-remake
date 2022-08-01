b46c933b36146355ab6975ba34d4781b9c07913e
Andrew Gomez <drewgomez209@gmail.com>
2022-08-01 14:04:49 -0700

    initial auth routes created

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,4 +3,13 @@ import { } from '../controllers/auth.js';
 
 const router = express.Router();
 
+// CREATE A USER
+router.post('/signup',)
+
+// SIGN IN
+router.post('/signin',)
+
+// GOOGLE AUTH
+router.post('/google',)
+
 export default router;
