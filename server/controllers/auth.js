9adbfb1bc5c10bdddb55d68e4936f64e98bcdadb
Andrew Gomez <drewgomez209@gmail.com>
2022-08-01 14:43:42 -0700

    cookie showing in insomnia on successful login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -35,7 +35,7 @@ export const signin = async (req, res, next) => {
 
         res.cookie('access_token', token, {
             httpOnly: true
-        }).status(200)
+        }).status(200).json(user)
     }
     catch (err) {
         next(err)
