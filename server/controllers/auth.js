c8cc290b674b8d5c197f11290784e0dab1640056
Andrew Gomez <drewgomez209@gmail.com>
2022-08-01 14:17:30 -0700

    added salt and hash to signup controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 export const signup = async (req, res) => {
-    console.log(req.body)
-    // try {
-    //     const newUser = new User(req.body)
-    // }
-    // catch (err) {
-    //     //todo
-    // }
+    // console.log(req.body)
+    try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+        const newUser = new User({ ...req.body, password: hash })
+    }
+    catch (err) {
+        //todo
+    }
 }
