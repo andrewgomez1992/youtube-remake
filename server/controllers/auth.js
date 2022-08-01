f964c8b570863555436a4fbd608f2877804c28c1
Andrew Gomez <drewgomez209@gmail.com>
2022-08-01 14:19:36 -0700

    user can be created

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,6 +8,9 @@ export const signup = async (req, res) => {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
         const newUser = new User({ ...req.body, password: hash })
+
+        await newUser.save()
+        res.status(200).json('User has been created')
     }
     catch (err) {
         //todo
