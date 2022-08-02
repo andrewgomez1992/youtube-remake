1ffc2852348edb0cd84dc7fe5024577ab3c2ca82
Andrew Gomez <drewgomez209@gmail.com>
2022-08-02 16:38:01 -0700

    addComment updated

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,7 +1,7 @@
 export const addComment = async (req, res, next) => {
     const newComment = new Comment({ ...req.body, userId: req.user.id })
     try {
-        const savedComment = await Comment.save()
+        const savedComment = await newComment.save()
         res.status(200).send(savedComment)
     }
     catch (err) {
