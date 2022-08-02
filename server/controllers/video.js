28830cc52f98344e55d9a02f99afedb9d50b14ba
Andrew Gomez <drewgomez209@gmail.com>
2022-08-02 16:13:43 -0700

    added split with comma to tags

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -109,6 +109,8 @@ export const sub = async (req, res, next) => {
 }
 
 export const getByTag = async (req, res, next) => {
+    const tags = req.query.tags.split(',') // splitting tags with commas
+    console.log(tags)
     try {
         const videos = await Video.find().sort({ views: -1 })
         res.status(200).json(videos)
