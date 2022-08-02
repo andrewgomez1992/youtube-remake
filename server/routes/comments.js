ce94892b3a23944c335f5b9d92b297b8297714a1
Andrew Gomez <drewgomez209@gmail.com>
2022-08-02 16:35:35 -0700

    controllers imported to routes for comments

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { } from '../controllers/comment.js';
 import { verifyToken } from '../verifyToken.js';
+import { addComment, deleteComment, getComments } from '../controllers/comment.js';
 
 const router = express.Router();
 
-router.post('/', verifyToken,)
+router.post('/', verifyToken, addComment)
+router.post('/', verifyToken, deleteComment)
+router.post('/', verifyToken, getComments)
 
 export default router;
