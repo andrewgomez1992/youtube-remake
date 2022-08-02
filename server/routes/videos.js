5b8d76090491e7ca5336e35a006a065158641d77
Andrew Gomez <drewgomez209@gmail.com>
2022-08-02 14:59:03 -0700

    imported middleware to videos

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import { } from '../controllers/video.js';
+import { verifyToken } from '../verifyToken.js';
+import { addVideo } from '../controllers/video.js';
 
 const router = express.Router();
+
+//create a video
+router.post('/', verifyToken, addVideo)
 
 export default router;
