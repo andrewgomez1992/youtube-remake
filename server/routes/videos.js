a59dd8c32c7e7cf8dcee82a8a00aacaaec54582e
Andrew Gomez <drewgomez209@gmail.com>
2022-08-02 16:11:20 -0700

    tags and search controllers imported to video route

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../verifyToken.js';
-import { addVideo, addView, trend, random, sub } from '../controllers/video.js';
+import { addVideo, addView, trend, random, sub, getByTag, search } from '../controllers/video.js';
 
 const router = express.Router();
 
@@ -13,7 +13,7 @@ router.put('/view/:id', addView)
 router.get('/trend', trend)
 router.get('/random', random)
 router.get('/sub', verifyToken, sub)
-router.get('/tags', sub)
-router.get('/search', sub)
+router.get('/tags', getByTag)
+router.get('/search', search)
 
 export default router;
