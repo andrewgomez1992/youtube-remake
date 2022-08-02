cc66339b90482bc863b462f7b3c011f5d62785a2
Andrew Gomez <drewgomez209@gmail.com>
2022-08-01 19:10:49 -0700

    update user finished

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,16 +1,40 @@
-import { createError } from "../error.js"
+import { createError } from "../error.js";
+import User from '../models/User.js'
 
 
-export const updateUser = (req, res, next) => {
+export const updateUser = async (req, res, next) => {
     if (req.params.id === req.user.id) {
-        //todo
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            }, {
+                new: true
+            })
+            res.status(200).json(updatedUser)
+        }
+        catch (err) {
+            next(err)
+        }
     } else {
         return next(createError(403, 'You can only update your account!'))
     }
 }
 
-export const deleteUser = (req, res, next) => {
+export const deleteUser = async (req, res, next) => {
+    if (req.params.id === req.user.id) {
+        try {
+            const updatedUser = await User.findByIdAndDelete(
+                req.params.id,
 
+            )
+            res.status(200).json('User has been deleted.')
+        }
+        catch (err) {
+            next(err)
+        }
+    } else {
+        return next(createError(403, 'You can only delete your account!'))
+    }
 }
 
 
