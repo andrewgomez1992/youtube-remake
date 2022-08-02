a15fcfba40f16c0cef07cdb39d5a57c75b9d234b
Andrew Gomez <drewgomez209@gmail.com>
2022-08-02 15:00:43 -0700

    video routers frame built

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -7,5 +7,8 @@ const router = express.Router();
 
 //create a video
 router.post('/', verifyToken, addVideo)
+router.put('/:id', verifyToken, addVideo)
+router.delete('/:id', verifyToken, addVideo)
+router.get('/find/:id', addVideo)
 
 export default router;
