bf7c8df5c34716d70020fac962bc2afc3e7629e8
Andrew Gomez <drewgomez209@gmail.com>
2022-08-02 14:52:38 -0700

    getUser finished

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -39,7 +39,8 @@ export const deleteUser = async (req, res, next) => {
 
 export const getUser = async (req, res, next) => {
     try {
-
+        const user = await User.findById(req.params.id)
+        res.status(200).json()
     }
     catch (err) {
         next(err)
@@ -48,7 +49,10 @@ export const getUser = async (req, res, next) => {
 
 export const subscribe = async (req, res, next) => {
     try {
-
+        await User.findById(req.user.id, {
+            $push: { subscribedUsers: req.params.id }
+        })
+        await User.findByIdAndUpdate(req.params.id)
     }
     catch (err) {
         next(err)
