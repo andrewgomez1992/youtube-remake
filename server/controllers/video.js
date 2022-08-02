6c3b0fbf5e6889df4ca11e0322240454e5985c1e
Andrew Gomez <drewgomez209@gmail.com>
2022-08-02 15:03:22 -0700

    addVideo finished

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,15 +1,37 @@
 export const addVideo = async (req, res, next) => {
-
+    const newVideo = new VideoColorSpace({ userId: req.user.id, ...req.body })
+    try {
+        const savedVideo = await newVideo.save()
+        res.status(200).json(savedVideo)
+    }
+    catch (err) {
+        next(err)
+    }
 }
 
 export const updateVideo = async (req, res, next) => {
+    try {
 
+    }
+    catch (err) {
+        next(err)
+    }
 }
 
 export const deleteVideo = async (req, res, next) => {
+    try {
 
+    }
+    catch (err) {
+        next(err)
+    }
 }
 
 export const getVideo = async (req, res, next) => {
+    try {
 
+    }
+    catch (err) {
+        next(err)
+    }
 }
