67f0e573891839db26e3a3f790f94a9304d80115
Andrew Gomez <drewgomez209@gmail.com>
2022-08-02 15:14:15 -0700

    updateVideo wip

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -15,7 +15,15 @@ export const addVideo = async (req, res, next) => {
 
 export const updateVideo = async (req, res, next) => {
     try {
-        const video = await Video.findById(req.params.id)
+        const video = await Video.findById(req.params.id);
+        if (!video) {
+            return next(createError(404, 'Video not found!'))
+        }
+        if (req.user.id === video.userId) {
+            const updatedUser = await Video.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            }, { new: true })
+        }
     }
     catch (err) {
         next(err)
