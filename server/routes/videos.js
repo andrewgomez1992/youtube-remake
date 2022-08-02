e33f0f8cc928befd3f38536f4a6a5b54896059ce
Andrew Gomez <drewgomez209@gmail.com>
2022-08-02 15:31:32 -0700

    added router endpoints for videos

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -9,5 +9,9 @@ router.post('/', verifyToken, addVideo)
 router.put('/:id', verifyToken, addVideo)
 router.delete('/:id', verifyToken, addVideo)
 router.get('/find/:id', addVideo)
+router.put('/view/:id', addVideo)
+router.get('/trend', addVideo)
+router.get('/random', addVideo)
+router.get('/sub', addVideo)
 
 export default router;
